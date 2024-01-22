1943fb2ce9af7c8c7b6091c91b88c4c9d9ab1fe2
Do Duy Bao <d98jay@outlook.com>
2024-01-23 05:19:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-/* ----- darkTheme ----- */
+/* ----- darkTheme v3 ----- */
 
-// check for saved 'themeConfig' in localStorage
-let themeConfig = localStorage.getItem('themeConfig');
+let themeConfig = localStorage.getItem('themeConfig') || 'system'; // Check local
 
-// set default value to 'themeConfig'
-if (themeConfig == null) {
-	localStorage.setItem('themeConfig', 'system');
-}
-
-// if themeConfig set to dark or system, change accordingly
-updateTheme(localStorage.themeConfig);
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-	if (e.matches && localStorage.themeConfig == 'system') {
-		document.documentElement.classList.add('dark');
-	}
-	if (!e.matches && localStorage.themeConfig == 'system') {
-		document.documentElement.classList.remove('dark');
-	}
-});
-
-// update the theme base on 'config' input and set value to 'themeConfig'
 function updateTheme(config) {
-	if (config == 'dark') {
-		document.documentElement.classList.add('dark');
-	}
-	if (config == 'light') {
-		document.documentElement.classList.remove('dark');
-	}
-	if (config == 'system') {
-		if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  		document.documentElement.classList.add('dark');
-		}
-		else {
-			document.documentElement.classList.remove('dark');
-		}
-	}
-	localStorage.setItem('themeConfig', config);
+  let turnDark = false;
+
+  if (config === 'dark') {
+    turnDark = true;
+  } else if (config === 'system') {
+    turnDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+  document.documentElement.classList.toggle('dark', turnDark);
+  localStorage.setItem('themeConfig', config);
+  themeConfig = config;
 }
 
+updateTheme(themeConfig); // Initial update theme
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+	if (themeConfig === 'system') {
+		document.documentElement.classList.toggle('dark', e.matches);
+	} // Update the theme when system color scheme changes
+});
